401dd4eb2ed0e3744d32add974f190852d02bdb3
ngopler <47447352+ngopler@users.noreply.github.com>
2025-08-09 14:54:23 +0700

    Update study-selection.py

--- study-selection.py
+++ study-selection.py
@@ -6,13 +6,18 @@ from sentence_transformers import SentenceTransformer
 import re
 from io import StringIO
 
-# Add custom CSS to hide the GitHub icon
-hide_github_icon = """
-#GithubIcon {
-  visibility: hidden;
-}
-"""
-st.markdown(hide_github_icon, unsafe_allow_html=True)
+st.markdown(
+    """
+    <style>
+    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
+    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
+    .viewerBadge_text__1JaDK {
+        display: none;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
 
 
 

6c1b9882597ad4d5924c1e8531978a179678efde
ngopler <47447352+ngopler@users.noreply.github.com>
2025-08-09 16:14:23 +0700

    Update study-selection.py

--- study-selection.py
+++ study-selection.py
@@ -7,16 +7,13 @@ import re
 from io import StringIO
 
 st.markdown(
-    """
+    r"""
     <style>
-    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
-    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
-    .viewerBadge_text__1JaDK {
-        display: none;
-    }
+    .stDeployButton {
+            visibility: hidden;
+        }
     </style>
-    """,
-    unsafe_allow_html=True
+    """, unsafe_allow_html=True
 )
 
 
